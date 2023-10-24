3d00b537c971d65850d1fab1df6e9eee250fa554
ASerene01 <122513988+ASerene01@users.noreply.github.com>
2023-10-24 12:21:51 +0545

    Signed-off-by: ASerene01 <122513988+ASerene01@users.noreply.github.com>

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Teacher, Student, RTE
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ["username", "email"]
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["username", "email"]
+
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(RTE)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfileBase(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    class Meta:
+        abstract = True
+
+
+class Teacher(UserProfileBase):
+    # Add customer-specific fields
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+
+
+class Student(UserProfileBase):
+    # Add employee-specific fields
+    employee_id = models.CharField(max_length=10)
+    job_title = models.CharField(max_length=50)
+
+
+class RTE(UserProfileBase):
+    # Add employee-specific fields
+    employee_id = models.CharField(max_length=10)
+    job_title = models.CharField(max_length=50)
